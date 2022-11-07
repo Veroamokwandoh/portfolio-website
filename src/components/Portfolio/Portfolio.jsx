ba105dc236cd6231d888b2d69b01b3819c3ab22b
Veroamokwandoh <vamokwandoh@st.ug.edu.gh>
2022-11-07 17:08:26 +0000

    modified

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Portfolio.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Sidebar from "../../img/sidebar.png";
+import Ecommerce from "../../img/ecommerce.png";
+import Hoc from "../../img/hoc.png";
+import MusicApp from "../../img/musicapp.png";
+// import 'swiper/css'
+
+function Portfolio() {
+  return (
+    <div className="Portfolio">
+      {/* Heading */}
+      <span>Recent Projects</span>
+      <span>Portfolio</span>
+
+      {/* slider */}
+      <Swiper>
+        <SwiperSlide>
+          <img src={Sidebar} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Ecommerce} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Hoc} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={MusicApp} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Portfolio;
--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './Works.css'
+import Upwork from "../../img/Upwork.png";
+import Fiverr from "../../img/fiverr.png";
+import Amazon from "../../img/amazon.png";
+import Shopify from "../../img/Shopify.png";
+import Facebook from "../../img/Facebook.png";
 
 function Works() {
   return (
-    <div>Works</div>
+    <div className="Works">
+      <div className="awesome">
+        <span>Works for All These</span>
+        <span>Brands & Clients</span>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.laudantium
+          libero
+          <br />
+          mollitia, ea modi et reprehenderit quisquam eveniet.
+        </span>
+        <button className="button s-button">Hire Me </button>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* Right side command */}
+      <div className="w-right">
+        <div className="w-mainCircle">
+          <div className="w-secCircle">
+            <img src={Upwork} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Fiverr} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Amazon} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Shopify} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Facebook} alt="" />
+          </div>
+        </div>
+        {/* Background circle */}
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
+      </div>
+    </div>
   )
 }
 
